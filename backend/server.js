959f65d937cc552cf6cdd30633156dc1f3233e27
Sophia <sophiawg143@gmail.com>
2025-05-15 23:31:54 -0400

    get token from spotify

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,23 @@ const app   = express();
 app.use(cors());
 app.use(express.json());
 
+const clientId = '93f0a39ae4d34f7a80fcc3cde2227940';
+const clientSecret = '1c260af380ec4980b57a6fe54b4c815b';
+
+const _getToken = async () => {
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic' + btoa(clientId + ':' + clientSecret),
+        },
+        body: 'grant_type=client_credentials',
+    });
+
+    const data = await result.json();
+    return data.access_token;
+}
+
 app.get( '/', (req, res) => {
     res.send("hello world from node.js server")
 });
