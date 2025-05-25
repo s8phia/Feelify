0dd3d6505ca806bac74905ab94d239df34bdc1e8
Sophia <sophiawg143@gmail.com>
2025-05-24 20:05:55 -0400

    basic song display ui

--- frontend/src/app/components/mood-selector/mood-selector.component.ts
+++ frontend/src/app/components/mood-selector/mood-selector.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SongService } from '../../song.service'; 
 import { Song } from '../../model/song.model'; 
+import { fromReadableStreamLike } from 'rxjs/internal/observable/innerFrom';
 
 @Component({
   selector: 'app-mood-selector',
@@ -13,16 +14,19 @@ export class MoodSelectorComponent {
   moods = ['happy', 'sad', 'angry', 'chill', 'excited', 'romantic'];
   selectedMood: string = '';
   songs: Song[] = [];
+  isLoading: boolean = false;
 
   constructor(private songService: SongService) {}
 
   selectMood(mood: string) {
     console.log('Selected mood:', mood);
     this.selectedMood = mood;
+    this.isLoading = true;
     this.songService.getSongsByMood(mood).subscribe({
       next: (data) => {
         console.log('Received songs:', data);
         this.songs = data.songs;
+        this.isLoading = false;
       },
       error: (error) => {
         console.error('Error fetching songs:', error);
